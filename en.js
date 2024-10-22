d72b02b0453a8f84356d442f9bc3ced076541c4e
kabirgaire0 <kabir.gaire123@gmail.com>
2024-10-22 12:46:51 +0900

    initial commit

---
+++ en.js
@@ -0,0 +1,40 @@
+const enContent = {
+    'name': 'Kabir Gaire',
+    'intro': '👋 Hello and welcome to my site. I am Kabir Gaire, a motivated engineer with diverse skills and a passion for technology.',
+    'backend-title': 'Backend Engineer',
+    'backend-desc': 'I am a Backend Engineer with a strong focus on building scalable, distributed systems using Go and AWS. My projects have involved microservices architecture, serverless frameworks, and cloud-native development, helping businesses create robust backend systems.',
+    'linkedin': 'LinkedIn',
+    'github': 'GitHub',
+    'portfolio': 'Portfolio',
+    'wantedly': 'Wantedly',
+    'fullstack-title': 'Full-Stack Developer',
+    'fullstack-desc': 'With experience as a Full-Stack Developer, I have worked with various technologies, including Laravel, React, and TypeScript. I strive for smooth integration between frontend and backend, ensuring a seamless user experience.',
+    'cloud-title': 'Cloud Infrastructure Enthusiast',
+    'cloud-desc': 'I am deeply interested in cloud technologies, including AWS and Kubernetes. I enjoy building and managing cloud-native infrastructure, focusing on automation, monitoring, and reliability.',
+    'skills-title': 'Skills & Tools',
+    'category-header': 'Category',
+    'skills-header': 'Skills & Tools',
+    'skills-table': {
+        'Languages': 'PHP, JavaScript, TypeScript, Go, Python',
+        'Frameworks': 'Laravel, Express.js, React.js, Django',
+        'Cloud Services': 'AWS (EC2, Lambda, API Gateway, RDS, DynamoDB, S3, etc), GCP, Stripe, SendGrid',
+        'Databases': 'MySQL, Redis',
+        'Tools': 'VSCode, Postman, MySQL Workbench, Git, Docker, Ubuntu'
+    },
+    'ambition-title': 'Ambition',
+    'ambition-list': [
+        'Deepen my expertise in backend development, cloud technologies, and system design',
+        'Become adept at quickly catching up with any new technology'
+    ],
+    'awards-title': 'Awards and Certifications',
+    'awards-list': [
+        'TOEIC Listening & Reading Test: 895 (Sep 2024)',
+        'Japanese Language Proficiency Test (JLPT): N1 (Dec 2021)',
+        'EIKEN Test in Practical English Proficiency: Pre-1st Grade (Oct 2019)'
+    ],
+    'hobbies-title': 'Hobbies',
+    'hobbies-list': [
+        'Walking, learning new technologies, coding, YouTube, cooking, chatting'
+    ],
+    'contact-title': 'Contact'
+};
---
+++ ja.js
@@ -0,0 +1,40 @@
+const jaContent = {
+    'name': 'カビル ガイレ',
+    'intro': '👋 こんにちは、私のサイトへようこそ。私はカビル ガイレです。多様なスキルと技術への情熱を持つエンジニアです。',
+    'backend-title': 'バックエンドエンジニア',
+    'backend-desc': '私はGoとAWSを使用してスケーラブルで分散型のシステムを構築することに注力しているバックエンドエンジニアです。プロジェクトにはマイクロサービスアーキテクチャ、サーバーレスフレームワーク、クラウドネイティブ開発が含まれ、企業が強固なバックエンドシステムを構築する手助けをしています。',
+    'linkedin': 'LinkedIn',
+    'github': 'GitHub',
+    'portfolio': 'Portfolio',
+    'wantedly': 'Wantedly',
+    'fullstack-title': 'フルスタックデベロッパー',
+    'fullstack-desc': 'フルスタックデベロッパーとしての経験があり、Laravel、Vue.js、React、TypeScriptなどのさまざまな技術を使用してきました。フロントエンドとバックエンドのスムーズな統合を目指し、シームレスなユーザー体験を提供しています。',
+    'cloud-title': 'クラウドインフラ愛好家',
+    'cloud-desc': 'AWSやKubernetesを含むクラウド技術に深い関心を持っています。クラウドネイティブなインフラの構築と管理を楽しんでおり、自動化、監視、信頼性に重点を置いています。',
+    'skills-title': 'スキルとツール',
+    'category-header': 'カテゴリー',
+    'skills-header': 'スキルとツール',
+    'skills-table': {
+      '言語': 'PHP, JavaScript, TypeScript, Go, Python',
+      'フレームワーク': 'Laravel, Express.js, React.js, Django',
+      'クラウドサービス': 'AWS (EC2, Lambda, API Gateway, RDS, DynamoDB, S3など), GCP, Stripe, SendGrid',
+      'データベース': 'MySQL, Redis',
+      'ツール': 'VSCode, Postman, MySQL Workbench, Git, Docker, Ubuntu'
+    },
+    'ambition-title': '目標',
+    'ambition-list': [
+      'バックエンド開発、クラウド技術、システム設計の専門知識を深める',
+      '新しい技術に素早くキャッチアップする能力を身につける'
+    ],
+    'awards-title': '受賞歴と資格',
+    'awards-list': [
+      'TOEICリスニング＆リーディングテスト：895点 (2024年9月)',
+      '日本語能力試験（JLPT）：N1 (2021年12月)',
+      '英検 実用英語技能検定：準1級 (2019年10月)'
+    ],
+    'hobbies-title': '趣味',
+    'hobbies-list': [
+      '散歩、技術勉強、コーディング、ユーチュブ、料理、雑談'
+    ],
+    'contact-title': '連絡先'
+  };
